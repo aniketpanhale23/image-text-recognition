90c89f779ffd4a11dc89d3b3efa41176ee051a05
aniketpanhale23 <60585451+aniketpanhale23@users.noreply.github.com>
2020-12-16 13:01:20 -0500

    Add files via upload

---
+++ src/ec2a.java
@@ -0,0 +1,116 @@
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicSessionCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.*;
+import com.amazonaws.services.rekognition.model.DetectLabelsRequest;
+import com.amazonaws.services.rekognition.model.DetectLabelsResult;
+import com.amazonaws.services.rekognition.model.Image;
+import com.amazonaws.services.rekognition.model.Label;
+import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.CreateQueueRequest;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.lang.System.exit;
+
+public class ec2a {
+
+    public static void main(String[] args) {
+        // Credentials for AWS Account
+        String aws_access_key_id = "ASIA5CFTBJTKV4CFWZNJ";
+        String aws_secret_access_key = "EmWggJuCq5cZvj+F619ZWI4+FxR5Kxq8vqYIFv5u";
+        String aws_session_token = "FwoGZXIvYXdzEFkaDMC3WgzkwaqtawK4HCK9Acf+n7pAd9yBF+HSBaAS8ys7uaD7thm/XV1NU46H8sLZNH1J8QYVRSpBErmVo2ogk56ZJbmX8XkwnK9MdBzdqQbLTyI7owRZgVSTTIFBRsgeWLwrvWtZsS8OYLT8qt4eb5lVpRkWH1jcPuN5QnXmaoDohStmCVvj5fLS3w8hceLaSIUMX19W8G6YrKgSUbldJ++LpmmO2M8EJUauRiorg/PoCCbVdmnCNXQgqlZkcoQc2O3akG3ho2L/IMRmjSjdnqnzBTItj2qrWNzVeAWhNx+MXsPWDNjqjzleN4PfgupxGR+2lNkmQwCwsSGBeMnByneW";
+
+        // Define variables
+        String BUCKET_NAME = "njit-cs-643";
+        String QUEUE_NAME = "AniketCCP.fifo";
+        String MESSAGE_GROUP_ID = "njit-cs-643-s3-images";
+
+        // Make a connection to the AWS Account
+        AWSCredentials credentials = new BasicSessionCredentials(aws_access_key_id, aws_secret_access_key, aws_session_token);
+        AWSStaticCredentialsProvider awsStaticCredentialsProvider = new AWSStaticCredentialsProvider(credentials);
+
+        // Make a connection to the AWS S3 Bucket
+        AmazonS3 s3 = AmazonS3ClientBuilder.standard()
+                .withCredentials(awsStaticCredentialsProvider)
+                .withRegion("us-east-1").build();
+
+        // Make a connection to the AWS Rekognition Service
+        AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder.standard()
+                .withCredentials(awsStaticCredentialsProvider)
+                .withRegion("us-east-1").build();
+
+        // Make a connection to the AWS SQS Service
+        AmazonSQS sqs = AmazonSQSClientBuilder.standard()
+                .withCredentials(awsStaticCredentialsProvider)
+                .withRegion("us-east-1").build();
+
+        ObjectListing objectListing = null;
+        try {
+            objectListing = s3.listObjects(new ListObjectsRequest().withBucketName(BUCKET_NAME));
+            Map<String, String> attributes = new HashMap<>();
+            attributes.put("FifoQueue", "true");
+            attributes.put("ContentBasedDeduplication", "true");
+            sqs.createQueue(new CreateQueueRequest().withQueueName(QUEUE_NAME).withAttributes(attributes));
+            String queueUrl = sqs.getQueueUrl(QUEUE_NAME).getQueueUrl();
+
+            for(S3ObjectSummary obj : objectListing.getObjectSummaries()) {
+                S3Object s3Object = s3.getObject(new GetObjectRequest(BUCKET_NAME, obj.getKey()));
+                System.out.println("Fetching image: " + obj.getKey() + " from the bucket: " + BUCKET_NAME);
+
+                // Detect cars from S3 Bucket
+                DetectLabelsRequest request = new DetectLabelsRequest()
+                        .withImage(new Image().withS3Object(new com.amazonaws.services.rekognition.model.S3Object().withName(s3Object.getKey()).withBucket(BUCKET_NAME)))
+                        .withMaxLabels(5).withMinConfidence(75F);
+                DetectLabelsResult result = rekognitionClient.detectLabels(request);
+
+                // Check if a car is detected
+                List<Label> labels = result.getLabels();
+                for (Label label : labels) {
+                    String detected = label.getName();
+                    Float confidence = label.getConfidence();
+                    System.out.println("Image: " + s3Object.getKey() + " Label: " + detected + " Confidence: " + confidence);
+                    if (detected.equals("Car")) {
+                        System.out.println("Detected a " + detected + " in " + s3Object.getKey() + " with confidence of " + confidence.toString() + "%");
+
+                        // Prepare the message to push in the FIFO Queue
+                        SendMessageRequest send_msg_request = new SendMessageRequest()
+                                .withQueueUrl(queueUrl)
+                                .withMessageBody(s3Object.getKey());
+                        send_msg_request.setMessageGroupId(MESSAGE_GROUP_ID);
+
+                        // Push the image name in the FIFO Queue
+                        sqs.sendMessage(send_msg_request);
+                        System.out.println("Pushed " + s3Object.getKey() + "in the SQS FIFO Queue " + QUEUE_NAME);
+                    }
+                }
+            }
+
+            // Append -1 at the end of the FIFO Queue
+            SendMessageRequest send_msg_request = new SendMessageRequest()
+                    .withQueueUrl(queueUrl)
+                    .withMessageBody("-1");
+            send_msg_request.setMessageGroupId(MESSAGE_GROUP_ID);
+
+            sqs.sendMessage(send_msg_request);
+
+        } catch (AmazonServiceException e) {
+            if (e.getErrorCode().equals("QueueAlreadyExists")) { }
+            else if (e.getErrorCode().equals("ExpiredToken")) {
+                System.out.println("Your credentials have expired. Please update aws_access_key_id, aws_secret_access_key and aws_session_token.");
+                exit(0);
+            } else {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/ec2b.java
@@ -0,0 +1,121 @@
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicSessionCredentials;
+import com.amazonaws.services.rekognition.model.*;
+import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.*;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static java.lang.System.exit;
+
+public class ec2b {
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+        // Credentials for AWS Account
+        String aws_access_key_id = "ASIA5CFTBJTKV4CFWZNJ";
+        String aws_secret_access_key = "EmWggJuCq5cZvj+F619ZWI4+FxR5Kxq8vqYIFv5u";
+        String aws_session_token = "FwoGZXIvYXdzEFkaDMC3WgzkwaqtawK4HCK9Acf+n7pAd9yBF+HSBaAS8ys7uaD7thm/XV1NU46H8sLZNH1J8QYVRSpBErmVo2ogk56ZJbmX8XkwnK9MdBzdqQbLTyI7owRZgVSTTIFBRsgeWLwrvWtZsS8OYLT8qt4eb5lVpRkWH1jcPuN5QnXmaoDohStmCVvj5fLS3w8hceLaSIUMX19W8G6YrKgSUbldJ++LpmmO2M8EJUauRiorg/PoCCbVdmnCNXQgqlZkcoQc2O3akG3ho2L/IMRmjSjdnqnzBTItj2qrWNzVeAWhNx+MXsPWDNjqjzleN4PfgupxGR+2lNkmQwCwsSGBeMnByneW";
+
+        // Define variables
+        String BUCKET_NAME = "njit-cs-643";
+        String QUEUE_NAME = "AniketCCP.fifo";
+        String OUTPUT_FILE = "output.txt";
+        boolean END_OF_QUEUE = Boolean.FALSE;
+
+        // Make a connection to the AWS Account
+        AWSCredentials credentials = new BasicSessionCredentials(aws_access_key_id, aws_secret_access_key, aws_session_token);
+        AWSStaticCredentialsProvider awsStaticCredentialsProvider = new AWSStaticCredentialsProvider(credentials);
+
+        // Make a connection to the AWS Rekognition Service
+        AmazonRekognition rekognitionClient = AmazonRekognitionClientBuilder.standard()
+                .withCredentials(awsStaticCredentialsProvider)
+                .withRegion("us-east-1").build();
+
+        // Make a connection to the AWS SQS Service
+        AmazonSQS sqs = AmazonSQSClientBuilder.standard()
+                .withCredentials(awsStaticCredentialsProvider)
+                .withRegion("us-east-1").build();
+
+        // Delete previous output file
+        Files.deleteIfExists(Paths.get(OUTPUT_FILE));
+
+        while(END_OF_QUEUE != Boolean.TRUE) {
+            try {
+                // Check if we have any messages available in the SQS FIFO Queue
+                ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(QUEUE_NAME);
+                List<Message> messages = sqs.receiveMessage(receiveMessageRequest)
+                        .getMessages();
+                Thread.sleep(1000);
+                System.out.println("Waiting for messages in "+QUEUE_NAME+"...");
+
+                for(Message message : messages) {
+                    if(!message.getBody().equals("-1")) {
+
+                        // Get the image from S3 and Detect Text
+                        DetectTextRequest request = new DetectTextRequest()
+                                .withImage(new Image()
+                                        .withS3Object(new com.amazonaws.services.rekognition.model.S3Object()
+                                                .withName(message.getBody())
+                                                .withBucket(BUCKET_NAME)));
+                        DetectTextResult result = rekognitionClient.detectText(request);
+                        List<TextDetection> textDetections = result.getTextDetections();
+                        System.out.println("Detected lines and words for " + message.getBody());
+
+                        // Print and save the text in the output file
+                        for (TextDetection text : textDetections) {
+                            if(text.getType().equals("LINE")) {
+                                String output = message.getBody() + " : " + text.getDetectedText() + "\n";
+                                byte b[] = output.getBytes();
+                                File outputFile = new File(OUTPUT_FILE);
+                                outputFile.createNewFile();
+                                FileOutputStream oFile = new FileOutputStream(outputFile, true);
+                                oFile.write(b);
+                                oFile.close();
+                                System.out.print(output);
+                            }
+                        }
+
+                        // Delete the message from the SQS FIFO Queue
+                        System.out.println("Deleting " + message.getBody() + " from " + QUEUE_NAME + ".");
+                        String messageReceiptHandle = message.getReceiptHandle();
+                        sqs.deleteMessage(new DeleteMessageRequest(QUEUE_NAME, messageReceiptHandle));
+                    } else {
+
+                        // Delete the queue if we have reached the end of the SQS FIFO Queue (Message "-1")
+                        System.out.println("Deleting "+QUEUE_NAME+".\nPlease wait for 60 seconds to run the scripts again...");
+                        sqs.deleteQueue(new DeleteQueueRequest(QUEUE_NAME));
+
+                        // Terminate the while loop
+                        END_OF_QUEUE = Boolean.TRUE;
+                    }
+                }
+            } catch (AmazonServiceException e) {
+                if (e.getErrorCode().equals("AWS.SimpleQueueService.NonExistentQueue")) {
+                    System.out.println("Queue " + QUEUE_NAME + " does not exist yet. Waiting for it to be created...");
+                    Thread.sleep(5000);
+                } else if (e.getErrorCode().equals("ExpiredToken")) {
+                    System.out.println("Your credentials have expired. Please update aws_access_key_id, aws_secret_access_key and aws_session_token.");
+                    exit(0);
+                } else {
+                    END_OF_QUEUE = Boolean.TRUE;
+                    e.printStackTrace();
+                }
+            } catch(InterruptedException i){
+                END_OF_QUEUE = Boolean.TRUE;
+                i.printStackTrace();
+            } catch (NullPointerException ignored) {} catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
